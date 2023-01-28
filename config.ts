d5554d3df6fc5233d6d5ae98ef861406f9368047
Firecakes <alberto19942@gmail.com>
2023-01-28 10:54:18 -0500

    Update version

--- config.ts
+++ config.ts
@@ -22,7 +22,7 @@ export let config = {
   serverCodeLength: Number(envs.SERVER_CODE_LENGTH) || 256,
   sslCertificateLocation: envs.HTTPS_CERTIFICATE_LOCATION,
   sslKeyLocation: envs.HTTPS_KEY_LOCATION,
-  version: "0.3.1"
+  version: "0.3.2"
 };
 
 if (config.title === undefined) {
