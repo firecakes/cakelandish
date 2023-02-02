079ca46b4df08a019d807e00ff6b7781d464890f
Firecakes <alberto19942@gmail.com>
2023-02-01 22:07:55 -0500

    Fix index misalignment when using feed list filters

--- config.ts
+++ config.ts
@@ -22,7 +22,7 @@ export let config = {
   serverCodeLength: Number(envs.SERVER_CODE_LENGTH) || 256,
   sslCertificateLocation: envs.HTTPS_CERTIFICATE_LOCATION,
   sslKeyLocation: envs.HTTPS_KEY_LOCATION,
-  version: "0.3.3"
+  version: "0.3.4"
 };
 
 if (config.title === undefined) {
