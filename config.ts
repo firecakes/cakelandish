d9635cf9a762fb132951304fb558b2b5dbb3ca25
Firecakes <alberto19942@gmail.com>
2023-02-02 22:25:02 -0500

    Version bump

--- config.ts
+++ config.ts
@@ -22,7 +22,7 @@ export let config = {
   serverCodeLength: Number(envs.SERVER_CODE_LENGTH) || 256,
   sslCertificateLocation: envs.HTTPS_CERTIFICATE_LOCATION,
   sslKeyLocation: envs.HTTPS_KEY_LOCATION,
-  version: "0.3.4"
+  version: "0.4.0"
 };
 
 if (config.title === undefined) {
