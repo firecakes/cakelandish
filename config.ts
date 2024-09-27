d5ff75c0a23b7863dd8a79f9f3861f1ac5300664
Firecakes <alberto19942@gmail.com>
2024-09-26 23:09:14 -0400

    Add collapse sidebar button for large screens

--- config.ts
+++ config.ts
@@ -15,7 +15,7 @@ try {
 }
 
 export const config = {
-  version: "0.12.0",
+  version: "0.13.0",
   title: envs.TITLE || "Cakelandish Feed",
   subtitle: envs.SUBTITLE,
   author: envs.AUTHOR || "Anonymous",
